1cedd00cf9bf606080e32a491eb0a7d76cc7bf52
unknown <gsqueo3@gmail.com>
2024-12-11 19:11:54 +0100

    Update barcharts.jsx

--- app/latest/components/barcharts.jsx
+++ app/latest/components/barcharts.jsx
@@ -6,6 +6,10 @@ import InfoUser from './infouser';
 function sumAmountsByMonth(transactions) {
     const sumsByMonth = {};
     
+    if (transactions.some(item => item.type).length <0 ) {
+      return []
+    }
+
     transactions.forEach((transaction) => {
       const date = new Date(transaction.date);
       const monthYear = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}`;
