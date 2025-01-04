2687c9eed7104bdc955028b9aa4bdd808b1a99cb
unknown <gsqueo3@gmail.com>
2025-01-04 11:21:24 +0100

    Update aboutus.jsx

--- app/components/aboutus.jsx
+++ app/components/aboutus.jsx
@@ -37,7 +37,7 @@ const AboutUs = () => {
         <h1 className='text-3xl font-extrabold  text-[#a9d6e5] py-1 italic'>La Nostra Storia</h1>
             <div className='text-white text-lg'>
                 Ehilà! Siamo Gabriele, Sara e Costantina, studenti universitari pugliesi. 
-                <br/>Abbaimo deciso di unirci per lavorare su un progetto innovativo che unisce tecnologia e creatività. 
+                <br/>Abbiamo deciso di unirci per lavorare su un progetto innovativo che unisce tecnologia e creatività. 
                 <p className='py-1'><br/>Io, Costantina sono appassionata di arte, mi occupo della grafica. Il mio obiettivo è rendere l’interfaccia del nostro progetto visivamente accattivante e intuitiva.</p>
                 <p className='my-1'><br/>Gabriele, sono esperto di sviluppo software, mi occupo dello scheletro e del software, assicurandomi che il progetto sia sempre funzionale e ben strutturato. 
                 </p><p className='my-1'><br/>Infine, Sara si concentra sul design e sull'esperienza utente,lavorando per rendere il nostro progetto non solo funzionale ma anche piacevole da utilizzare, garantendo semplicità e accessibilità.
